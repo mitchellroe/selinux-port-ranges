163289080d6ab6d8d0e06f022955dac3ffbaef2f
Mitchell Roe <mbroe@oakland.edu>
2020-01-08 17:47:15 -0500

    Found the issue.  Non-ranged port numbers aren't being counted.

--- selinux_ports.py
+++ selinux_ports.py
@@ -9,25 +9,39 @@ highest_port = 65535
 
 def main():
     raw_output = _get_raw_output()
+    _print_first_few(raw_output)
+    print()
+
     first_pass = _filter_tcp_ports(raw_output)
+    _print_first_few(first_pass)
+    print()
+
     del raw_output
+
     second_pass = _split_up_sequences(first_pass)
+    _print_first_few(second_pass)
+    print()
+
     del first_pass
-    reserved_ports = sorted(_expand_ranges(second_pass))
+
+    reserved_ports = _expand_ranges(second_pass)
+    _print_first_few(reserved_ports)
+    print()
+
     del second_pass
 
-    for x in range(0, len(reserved_ports)):
-        y = x + 1
-        if y == len(reserved_ports) - 1:
-            break
-        current_port = reserved_ports[x]
-        next_port = reserved_ports[y]
-        if next_port == current_port + 1:
-            continue
-        else:
-            print("low: " + str(current_port))
-            print("high: " + str(next_port))
-            print()
+    # for x in range(0, len(reserved_ports)):
+    #     y = x + 1
+    #     if y == len(reserved_ports) - 1:
+    #         break
+    #     current_port = reserved_ports[x]
+    #     next_port = reserved_ports[y]
+    #     if next_port == current_port + 1:
+    #         continue
+    #     else:
+    #         print("low: " + str(current_port))
+    #         print("high: " + str(next_port))
+    #         print()
 
 
 def _get_raw_output():
@@ -61,6 +75,7 @@ def _filter_tcp_ports(my_array):
 
 def _split_up_sequences(my_array):
     """Split up lines with more than one port."""
+    # TODO: This doesn't seem to include non-ranged port numbers.
     retval = []
     for entry in my_array:
         for port_field in entry.split('|'):
@@ -96,6 +111,11 @@ def _is_within_range(port_number):
         return True
     else:
         return False
+
+
+def _print_first_few(my_array):
+    for i in range(0, 15):
+        print(str(my_array[i]))
 
 
 if __name__ == '__main__':
